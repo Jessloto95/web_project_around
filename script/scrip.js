896ff49a814536cf9ae7ccb1f072d368fe10360c
JessFlores <jesskarinloto@gmail.com>
2025-06-13 10:01:42 -0600

    Etapa popup final

--- script/scrip.js
+++ script/scrip.js
@@ -1,30 +1,33 @@
 
 let form = document.querySelector(".popup__content")
-
-form.addEventListener('submit', save);
-
-function save(evt) {console.log("funciona")}
-
-
+let inputName = document.querySelector(".popup__input_name")
+let inputHobbie = document.querySelector(".popup__input_hobbie")
+let infName = document.querySelector(".profile__name")
+let infHobbie =document.querySelector(".profile__hobbie")
 let butEdit = document.querySelector(".profile__edit-button")
-
-butEdit.addEventListener('click', open);
+let butClose = document.querySelector(".popup__button_close")
 
 function open(evt) { 
     let popup = document.querySelector(".popup")
+    inputName.value = infName.textContent;
+    inputName.value = infHobbie.textContent;
     popup.classList.add("popup_opened")
 }
 
-
-
-let butClose = document.querySelector(".popup__button_close")
-
-butClose.addEventListener('click', close);
-
+function save(evt) {
+    evt.preventDefault();
+    infName.textContent = inputName.value;
+    infHobbie.textContent = inputHobbie.value;
+    close()
+}
 function close(evt) {
     let popup = document.querySelector(".popup")
     popup.classList.remove("popup_opened")
 }
+
+butEdit.addEventListener('click', open);
+form.addEventListener('submit', save);
+butClose.addEventListener('click', close);
 
 
 //let formElement = document.querySelector(".popup__content")
